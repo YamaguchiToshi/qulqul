9fb21b982f65cf6a5954bf611d53e4b0f465d7a2
Toshimitsu Yamaguchi <toshi.yamaguchi@eng.niigata-u.ac.jp>
2017-10-08 22:56:09 +0900

    複数の写真の切り替えをサポート

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,9 @@
 (function() {
 
-	var camera, scene, renderer, material;
+	var camera, scene, renderer;
+	var materials = new Array(30);
+	var current = 0;
+	var maxMaterial = 0;
 
 	var fov = 70,
 		texture_placeholder,
@@ -33,12 +36,18 @@
 		var geometry = new THREE.SphereGeometry(500, 60, 40);
 		geometry.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
 
-		material = new THREE.MeshBasicMaterial({
-			map: THREE.ImageUtils.loadTexture('./images/textures/image05.jpg')
+		$.ajaxSetup({async: false});
+		$.getJSON("./javascripts/config.json", function(data) {
+			maxMaterial = data.length;
+			for(var i = 0; i < maxMaterial; i++){
+				materials[i] = new THREE.MeshBasicMaterial({
+					map: THREE.ImageUtils.loadTexture('./images/textures/' + data[i])
+				});
+			}
 		});
+		$.ajaxSetup({async: true});
 
-		mesh = new THREE.Mesh(geometry, material);
-
+		mesh = new THREE.Mesh(geometry, materials[0]);
 		scene.add(mesh);
 
 		renderer = new THREE.WebGLRenderer();
@@ -49,6 +58,7 @@
 		document.addEventListener('mousemove', onDocumentMouseMove, false);
 		document.addEventListener('mouseup', onDocumentMouseUp, false);
 		document.addEventListener('mousewheel', onDocumentMouseWheel, false);
+		document.addEventListener('keydown', onDocumentKeyDown, false);
 		document.addEventListener('DOMMouseScroll', onDocumentMouseWheel, false);
 
 		//
@@ -82,7 +92,7 @@
 
 	function onDocumentMouseMove(event) {
 
-		deltaLonMax = 0.09;
+		deltaLonMax = 0.1;
 		w = window.innerWidth;
 		if( event.clientX < w / 4 ){
 			deltaLon = (deltaLonMax * event.clientX) / (w/4) - deltaLonMax;
@@ -102,8 +112,8 @@
 			deltaLat = 0.0;
 		}
 
-		now = new Date();
-		console.log(now.toISOString()+","+event.clientX + "," + event.clientY);
+		//now = new Date();
+		//console.log(now.toISOString()+","+event.clientX + "," + event.clientY);
 
 
 		if (isUserInteracting) {
@@ -123,21 +133,13 @@
 		// WebKit
 
 		if (event.wheelDeltaY) {
-
 			fov -= event.wheelDeltaY * 0.05;
-
 			// Opera / Explorer 9
-
 		} else if (event.wheelDelta) {
-
 			fov -= event.wheelDelta * 0.05;
-
 			// Firefox
-
 		} else if (event.detail) {
-
 			fov += event.detail * 1.0;
-
 		}
 
 		camera.projectionMatrix.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
@@ -145,15 +147,37 @@
 
 	}
 
-	function animate() {
+	function onDocumentKeyDown(event){
+		var keycode = event.keyCode;
+		switch(keycode){
+			case 38 :
+			current--;
+			if( current >= 0 ){
+				scene.overrideMaterial = materials[current];
+			} else {
+				current = maxMaterial-1;
+				console.log(maxMaterial);
+				scene.overrideMaterial = materials[current];
+			}
+			break;
+			case 40 :
+			current++;
+			if( current < maxMaterial ){
+				scene.overrideMaterial = materials[current];
+			} else {
+				current = 0;
+				scene.overrideMaterial = materials[current];
+			}
+			break;
+		}
+	}
 
+	function animate() {
 		requestAnimationFrame(animate);
 		render();
-
 	}
 
 	function render() {
-
 		lat = Math.max(-85, Math.min(85, lat));
 		phi = THREE.Math.degToRad(90 - lat);
 		theta = THREE.Math.degToRad(lon);
@@ -161,15 +185,7 @@
 		camera.target.x = 500 * Math.sin(phi) * Math.cos(theta);
 		camera.target.y = 500 * Math.cos(phi);
 		camera.target.z = 500 * Math.sin(phi) * Math.sin(theta);
-
 		camera.lookAt(camera.target);
-
-		/*
-				// distortion
-				camera.position.x = - camera.target.x;
-				camera.position.y = - camera.target.y;
-				camera.position.z = - camera.target.z;
-				*/
 
 		renderer.render(scene, camera);
 
